e74c315bca452b4083362001924bcf8470710547
Andrew Werner <drewwerner@Andrews-MacBook-Pro.local>
2020-05-14 19:09:29 -0600

    workout.js

---
+++ Develop/models/workout.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const workoutSchema = new mongoose.Schema({
+    day: {
+        type: Date,
+        required: true
+    },
+    exerises: [
+        {
+            type: {
+                type: String,
+                required: true
+            },
+       
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+
+            duration: {
+                type: Number,
+                required: true
+            },
+
+            weight: {
+                type: Number
+            },
+
+            reps: {
+                type: Number
+            },
+
+            sets: {
+                type: Number
+            },
+
+            distance: {
+                type: Number
+            }
+        }
+    ],
+});
+
+
+//creating the Model
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
